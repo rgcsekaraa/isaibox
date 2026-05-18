2f3880a8716ce3d4cd428b8a3bc53e8b7da7df0a
Chan RG <rgchandrasekaraa@gmail.com>
2026-05-18 20:15:37 +1000

    fix: keep footer visible with expanded mobile player

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "isaibox-shell-v8";
+const CACHE_NAME = "isaibox-shell-v9";
 const SHELL_URLS = ["/", "/site.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
